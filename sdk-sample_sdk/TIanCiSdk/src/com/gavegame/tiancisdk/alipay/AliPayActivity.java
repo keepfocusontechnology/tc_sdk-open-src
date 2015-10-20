f8ce643c7f191a2d6367e37d2b046e7a187b743d
zy <zhangying@gavegame.com>
2015-10-20 13:30:19 +0800

    去除v7包

--- sdk-sample_sdk/TIanCiSdk/src/com/gavegame/tiancisdk/alipay/AliPayActivity.java
+++ sdk-sample_sdk/TIanCiSdk/src/com/gavegame/tiancisdk/alipay/AliPayActivity.java
@@ -50,6 +50,7 @@ public class AliPayActivity extends FragmentActivity {
 				if (TextUtils.equals(resultStatus, "9000")) {
 					Toast.makeText(AliPayActivity.this, "支付成功",
 							Toast.LENGTH_SHORT).show();
+					
 				} else {
 					// 判断resultStatus 为非“9000”则代表可能支付失败
 					// “8000”代表支付结果因为支付渠道原因或者系统原因还在等待支付结果确认，最终交易是否成功以服务端异步通知为准（小概率状态）
--- sdk-sample_sdk/tiancisdkDemo/src/com/gavegame/tiancisdkdemo/MainActivity.java
+++ sdk-sample_sdk/tiancisdkDemo/src/com/gavegame/tiancisdkdemo/MainActivity.java
@@ -1,13 +1,12 @@
 package com.gavegame.tiancisdkdemo;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Toast;
 
 import com.gavegame.tiancisdk.Config;
@@ -18,13 +17,14 @@ import com.gavegame.tiancisdk.network.RequestCallBack;
 import com.gavegame.tiancisdk.network.ResponseMsg;
 import com.gavegame.tiancisdk.utils.TCLogUtils;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	private Context context;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
 		context = getApplicationContext();
 		
@@ -127,7 +127,11 @@ public class MainActivity extends ActionBarActivity {
 			
 			@Override
 			public void onClick(View v) {
-				startActivity(new Intent(MainActivity.this,AliPayActivity.class));
+				Intent intent = new Intent(MainActivity.this,AliPayActivity.class);
+				intent.putExtra("subject", "商品名称");
+				intent.putExtra("subject_desc", "商品详情");
+				intent.putExtra("price", "0.01");
+				startActivityForResult(intent,0);
 			}
 		});
 	}
@@ -151,22 +155,4 @@ public class MainActivity extends ActionBarActivity {
 		TianCi.init(this);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
