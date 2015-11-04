983a0a54cb9c8ec498cb28e987dc7b894fa263b8
zy <zhangying@gavegame.com>
2015-11-04 11:27:04 +0800

    主干取出v7包

--- sdk-sample_sdk/tiancisdkDemo/src/com/gavegame/tiancisdkdemo/MainActivity.java
+++ sdk-sample_sdk/tiancisdkDemo/src/com/gavegame/tiancisdkdemo/MainActivity.java
@@ -1,11 +1,9 @@
 package com.gavegame.tiancisdkdemo;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
@@ -17,7 +15,7 @@ import com.gavegame.tiancisdk.network.RequestCallBack;
 import com.gavegame.tiancisdk.network.ResponseMsg;
 import com.gavegame.tiancisdk.utils.TCLogUtils;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	private Context context;
 	
@@ -140,24 +138,5 @@ public class MainActivity extends ActionBarActivity {
 		// TODO Auto-generated method stub
 		super.onResume();
 		TianCi.init(this);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
